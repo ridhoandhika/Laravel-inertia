11ed74fa413240bcb3c6e637dbab5d3ca8b7ee19
ridhoandhika <ridhoandhika74@gmail.com>
2025-06-14 15:48:32 +0700

    Remove comment

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,20 +17,19 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         // Contoh data base64 dari string acak (bisa dianggap sebagai isi file dummy)
-        // $fileContent = base64_encode('Ini adalah konten file dummy ' . $this->faker->uuid);
-   $imageUrl = 'https://th.bing.com/th/id/OIP.m39aI7v6Mgsk2r_ozkpVBQHaG6?r=0&rs=1&pid=ImgDetMain&cb=idpwebp1&o=7&rm=3'; // Random image 200x200
+        $imageUrl = 'https://th.bing.com/th/id/OIP.m39aI7v6Mgsk2r_ozkpVBQHaG6?r=0&rs=1&pid=ImgDetMain&cb=idpwebp1&o=7&rm=3'; // Random image 200x200
 
-    // Ambil konten gambar dari URL
-    $imageContent = file_get_contents($imageUrl);
+        // Ambil konten gambar dari URL
+        $imageContent = file_get_contents($imageUrl);
 
-    // Konversi ke base64
-    $fileContent = base64_encode($imageContent);
-        return [
-            'name' => $this->faker->words(2, true),
-            'description' => $this->faker->paragraph(),
-            'price' => $this->faker->randomFloat(2, 10, 1000),
-            'stock' => $this->faker->numberBetween(1, 100),
-            'file' => $fileContent,
-        ];
+        // Konversi ke base64
+        $fileContent = base64_encode($imageContent);
+            return [
+                'name' => $this->faker->words(2, true),
+                'description' => $this->faker->paragraph(),
+                'price' => $this->faker->randomFloat(2, 10, 1000),
+                'stock' => $this->faker->numberBetween(1, 100),
+                'file' => $fileContent,
+            ];
     }
 }
